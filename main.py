49c62cae3c29031bc5558e22de2f70569bc8ec7b
Alfie Fitch <alfiefitch0@gmail.com>
2023-04-04 12:32:23 +0000

    Added size of file to embed, and added extra checks to download

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from StringProgressBar import progressBar
 
 qbt_client = qbittorrentapi.Client(
     host='<hostname>', 
-    port = <port_number>,
-    username='<username>', 
-    password='<password>'
+    port = <port>,
+    username='<username', 
+    password='<password'
     )
 
 films = []
@@ -29,17 +29,24 @@ class MyClient(discord.Client):
         if message.content == 'check':
             await check_torrents()
 
+def convert_size(size_bytes):
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return "%s %s" % (s, size_name[i])
+
 async def check_torrents():
     print("checking new torrents")
-    print(films)
-    channel = client.get_channel(890756113354616853)
+    channel = client.get_channel(<channel_id>)
     for torrent in qbt_client.torrents_info():
-        print(torrent.name)
 
         present = False;
 
         for x in films:
-            if(torrent.name == x["name"]):
+            if(torrent.name == x["name"] and torrent.size == x["size"]):
                 present = True;
                 break;
         
@@ -50,6 +57,7 @@ async def check_torrents():
                 embed = discord.Embed(title="Download Complete", description="", color=0x00ff00)
                 embed.add_field(name="Title", value=torrent.name, inline=False)
                 embed.add_field(name="Progress", value=bardata[0] + " " + str(math.floor(bardata[1])) + "%", inline=False)
+                embed.add_field(name="Size", value = convert_size(torrent.size), inline=False)
                 message = x["message_id"]
                 await channel.get_partial_message(message).edit(embed=embed)
             else:
@@ -58,6 +66,7 @@ async def check_torrents():
                 embed = discord.Embed(title="New Download", description="", color=0xe61eb1)
                 embed.add_field(name="Title", value=torrent.name, inline=False)
                 embed.add_field(name="Progress", value=bardata[0] + " " + str(math.floor(bardata[1])) + "%", inline=False)
+                embed.add_field(name="Size", value = convert_size(torrent.size), inline=False)
                 message = x["message_id"]
                 await channel.get_partial_message(message).edit(embed=embed)
 
@@ -67,8 +76,9 @@ async def check_torrents():
             embed = discord.Embed(title="New Download", description="", color=0xe61eb1)
             embed.add_field(name="Title", value=torrent.name, inline=False)
             embed.add_field(name="Progress", value=bardata[0] + " " + str(math.floor(bardata[1])) + "%", inline=False)
+            embed.add_field(name="Size", value = convert_size(torrent.size), inline=False)
             message = await channel.send(embed=embed)
-            x = {"name": torrent.name, "message_id": message.id}
+            x = {"name": torrent.name, "message_id": message.id, "size": torrent.size}
             films.append(x)
 
         if not films:
@@ -77,12 +87,13 @@ async def check_torrents():
             embed = discord.Embed(title="New Download", description="", color=0xe61eb1)
             embed.add_field(name="Title", value=torrent.name, inline=False)
             embed.add_field(name="Progress", value=bardata[0] + " " + str(math.floor(bardata[1])) + "%", inline=False)
+            embed.add_field(name="Size", value = convert_size(torrent.size), inline=False)
             message = await channel.send(embed=embed)
             x = {"name": torrent.name, "message_id": message.id}
             films.append(x)
 
 intents = discord.Intents.default()
-intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run("Discord Token Here")
+client.run("<bot_token>")
+
